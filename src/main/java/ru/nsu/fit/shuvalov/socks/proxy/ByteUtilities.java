e79efb42e7a711062e58be9b2a190c8dee19f965
mentalMint <walkershush@gmail.com>
2023-01-12 15:13:05 +0700

    feat: make Socks5ProxyServer chore: remove needless modules

--- src/main/java/ru/nsu/fit/shuvalov/socks/proxy/ByteUtilities.java
+++ src/main/java/ru/nsu/fit/shuvalov/socks/proxy/ByteUtilities.java
@@ -5,6 +5,17 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 
 public class ByteUtilities {
+    public static boolean contains(byte[] arr, byte toCheckValue) {
+        boolean test = false;
+        for (int element : arr) {
+            if (element == toCheckValue) {
+                test = true;
+                break;
+            }
+        }
+        return test;
+    }
+
     static public byte[] concatenate(byte[] a, byte[] b) throws IOException {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         outputStream.write(a);
